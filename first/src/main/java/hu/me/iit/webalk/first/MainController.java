4c3eeb00b5ef2d3b9d5b46ebf566f97191146cda
kalodaniel <kalodaniel@192.168.1.71>
2021-09-30 16:26:40 +0200

    elso Spring Project feltoltese git segitsegevel

---
+++ first/src/main/java/hu/me/iit/webalk/first/MainController.java
@@ -0,0 +1,22 @@
+package hu.me.iit.webalk.first;
+
+import java.awt.PageAttributes.MediaType;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+	
+	@GetMapping(path="/", produces=org.springframework.http.MediaType.APPLICATION_JSON_VALUE)
+	@ResponseBody
+	ResponseDto gyoker() {
+		ResponseDto responseDto = new ResponseDto();
+		
+		responseDto.setId(123);
+		responseDto.setMessage("Helloo");
+		return responseDto;
+	}
+	
+}

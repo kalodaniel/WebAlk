f91ed621b0c87b84103405ca3d6624f0a2b88c06
kalodaniel <kalodaniel@localhost>
2021-09-30 16:58:30 +0200

    masodik spring alkalmazasom

---
+++ second/src/main/java/hu/uni/me/iit/second/ResponseDto.java
@@ -0,0 +1,22 @@
+package hu.uni.me.iit.second;
+
+public class ResponseDto {
+	
+	int id;
+	String message;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	
+}
